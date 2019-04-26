dda212dc48aac279fd4b6433a4c15bc7307beb5b
AbulAsar <abul.a@kiprosh.com>
2019-04-26 21:20:36 +0530

    add style for loading spinner

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,14 +1,14 @@
 import Ember from "ember";
+import _ from "lodash";
 
 export default Ember.Controller.extend({
-  count: 1,
-
+  
   generateFakeData() {
     let data = [];
     const NAMES = ["Bruce Banner", "Captain America", "Agent Fury", "Tony Stark", "Clint", "Natasha", "Sam Wilson", "Thor", "Thanos", "Rocket", "Col. Rhodes"];
     
     for (var i = 1; i <= 25; i++) {
-      data.push({ id: this.count++, name: NAMES[Math.floor(Math.random() * NAMES.length)] });
+      data.push({ name: NAMES[_.random(0, NAMES.length-1)] });
     }
     return data;
   },
